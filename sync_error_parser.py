058de2d6e0fa5406e30e71818f14ace4860e6b10
Harald Klein <hari@vt100.at>
2021-10-21 10:57:22 +0200

    cleanup

--- sync_error_parser.py
+++ sync_error_parser.py
@@ -21,9 +21,3 @@ for shard in error_list:
 
 	for entry in shard['entries']:
 		print entry['timestamp'], shard_id, entry['section'], '"' + entry['name'] + '"', entry['info']['error_code'], '"' + entry['info']['message'] + '"'
-		# current_type_ts = datetime.strptime(type['time'], '%Y-%m-%d %H:%M:%S.%f') # 2017-01-27 16:25:06.942698
-		# if 'last_type_ts' in locals():
-		#	delta = current_type_ts - last_type_ts
-	#		total_type_duration += delta
-		#print "   ", "%d.%.6d" % (total_type_duration.seconds, total_type_duration.microseconds), "%d.%.6d" % (delta.seconds, delta.microseconds), type['event']
-		#pprint(type)
